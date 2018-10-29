19e5cfb5a5b243f078f3f836a3b740a834463596
reginaongowarsito <regina.ongowarsito@gmail.com>
2018-10-28 21:36:29 -0700

    initial commit + working part 1, only confirmed for chess though

---
+++ utils.py
@@ -0,0 +1,57 @@
+from skimage.io import imsave, imshow, show
+import numpy as np
+import datetime
+import os
+import skimage.io as skio
+import skimage as sk
+import imageio
+
+import matplotlib.pyplot as plt
+
+fOutputDirectory = "output_imgs"
+fFormat = ".jpg"
+
+#IMAGE IO
+TESTING_DIR = "trash_imgs"
+
+def timeStampPath(path):
+    currTime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    finalPath = "%s_%s" % (currTime, path)
+
+    return finalPath
+
+def printImage(name, im, disp=False, directory = fOutputDirectory):
+    # save the image
+
+    finalPath = directory + "/"+ timeStampPath(name)
+    imsave(finalPath, im)
+
+    print("File saved to:", finalPath)
+
+    # display the image
+    if disp:
+        viewImage(im)
+
+def testImage(path, im, disp=True):
+    finalPath = path
+
+    printImage(TESTING_DIR, finalPath, im, disp)
+
+def viewImage(im):
+    imshow(im)
+    show()
+
+def grayscale2RGB(im):
+    assert im.ndim == 2
+
+    return np.dstack((np.dstack((im, im)), im))
+
+def readImageNName(impath1):
+    imname1 = "".join(os.path.basename(impath1).split(".")[:-1])
+    im1 = skio.imread(impath1)
+    im1 = sk.img_as_float(im1)
+    return im1, imname1
+
+def convertToGif(fileName, lstImArr):
+    imageio.mimsave('./%s/%s.gif' % (fOutputDirectory, fileName), [im.imArr for im in lstImArr])
+    return
